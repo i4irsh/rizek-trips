b94d2eb2312670d4d604e4931f912b8d259a1f06
Irshad S <i4irsh@gmail.com>
2021-08-22 01:45:09 +0530

    code cleanup removed unused package - cookie-parser

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const exphbs = require('express-handlebars');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
@@ -20,7 +19,6 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'data')));
 
